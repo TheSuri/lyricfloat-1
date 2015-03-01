38affb0dfcb149ccb55a4bf94a84422b67317191
Jiayang Miao <jiayang.miao@gmail.com>
2015-03-01 03:40:41 -0800

    Facebook share working
    Facebook sharing code integrated into word-cloud.php, a newer open graph
    sharing method is included in word-cloud_backup.html but is not
    currently working (probably need the application to be hosted somewhere
    to test).

--- word-cloud.php
+++ word-cloud.php
@@ -59,7 +59,47 @@
 						</div>
 						<div class="inner-wrap">
 						   <button class="third-button" type="submit">Add To Cloud</button>	
-						   <button class="fb-third-button" type="submit">TEMP</button>	
+							
+							<div id="fb-root"></div>
+						   	<script>
+								  window.fbAsyncInit = function() {
+									FB.init({
+									  appId  : '1422647264695991',
+									  status : true, // check login status
+									  cookie : true, // enable cookies to allow the server to access the session
+									  xfbml  : true  // parse XFBML
+									});
+								  };
+								
+								  (function() {
+									var e = document.createElement('script');
+									e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+									e.async = true;
+									document.getElementById('fb-root').appendChild(e);
+								  }());
+								</script>
+
+							<script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js" type="text/javascript"></script>
+
+							<img id="share_button" src="share_button.png" >
+												
+							<script type="text/javascript">
+								$(document).ready(function(){
+								$('#share_button').click(function(e){
+								e.preventDefault();
+								FB.ui(
+								{
+								method: 'feed',
+								name: 'LyricFloat word cloud!',
+								link: ' http://www.mushroomsensei.com/',
+								picture: 'http://www.osamuko.com/blog/wp-content/uploads/2009/09/8.jpg',
+								caption: 'Come and see the word cloud I generated.',
+								description: 'Such Lyric, so float, much cloud.',
+								message: ''
+								});
+								});
+								});
+							</script>
 						   <button class="third-button" type="submit">Submit</button>		
 						</div>
 					</form>
