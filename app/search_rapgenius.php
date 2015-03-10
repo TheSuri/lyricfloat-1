7a8493c575693f1604be5027aa67cf40634faa4a
Tobias Lee <tobiasle@usc.edu>
2015-03-10 12:49:03 -0700

    Minor OCD

--- app/search_rapgenius.php
+++ app/search_rapgenius.php
@@ -5,23 +5,22 @@
  * Date: 2/25/2015
  * Time: 10:42 AM
  */
-
 require_once(__DIR__.'/../RapGenius-PHP-master/src/rapgenius.php');
 require_once(__DIR__.'/../RapGenius-PHP-master/src/rap_genius_wrapper.php');
-
 
 function getLyrics($artist_results, $rapgenius)
 {
     $artists = array();
-    foreach ($artist_results as $artist) {
+    foreach ($artist_results as $artist)
+	{
         $artists[$artist] = array();
-        $song_results = $rapgenius->param_q(array('q' => $artist))
-            ->execute_request();
-
+        $song_results = $rapgenius->param_q(array('q' => $artist))->execute_request();
         $songs = array();
 
-        foreach ($song_results as $song) {
-			if ($song['result']['primary_artist']['name'] == $artist) {
+        foreach ($song_results as $song)
+		{
+			if ($song['result']['primary_artist']['name'] == $artist)
+			{
 				$songs[] = array(
 					'title' => $song['result']['title'],
 					'id' => $song['result']['id'],
@@ -31,19 +30,8 @@ function getLyrics($artist_results, $rapgenius)
         }
         $artists[$artist] = $songs;
     }
-
+	
     return $artists;
 }
-
-
-
-
-//$songs = array();
-//foreach($albums as $album) {
-//    $songs[] = tracklist($album['link']);
-//}
-//
-//print_r($songs);
-
 ?>
 
