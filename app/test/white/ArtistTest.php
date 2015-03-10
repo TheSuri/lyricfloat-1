90220c9e532f423307600a94a03c811fa374ee8d
Ryan Lu <ryanlu994@gmail.com>
2015-03-09 21:48:32 -0700

    2 more test files

---
+++ app/test/white/ArtistTest.php
@@ -0,0 +1,19 @@
+<?php
+require_once('../../Artist.php');
+
+class ArtistTest extends PHPUnit_Framework_TestCase
+{	
+	public function testArtistCreation()
+	{
+		$artist = new Artist('name');
+		$this->assertEquals('name', $artist->name);
+		
+
+	}
+	
+	public function testArtistCreationFailure()
+	{
+		
+	}
+}
+?>
