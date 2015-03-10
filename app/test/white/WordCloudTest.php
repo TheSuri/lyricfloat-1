9d9a101a8e572b44346e1b706a67b28d7abdbc9b
Janet Kim <janet.hyesun.kim@gmail.com>
2015-03-10 01:49:39 -0700

    Changed privacy settings to public for WordCloud. Generated test case for filter_stopwords() and for standardDeviation(). Have not yet generated unit test for generateWC, getSongsWith or for shuffle_assoc

--- app/test/white/WordCloudTest.php
+++ app/test/white/WordCloudTest.php
@@ -6,6 +6,7 @@ class WordCloudTest extends PHPUnit_Framework_TestCase
 	
 
 	protected $artist;
+	protected $mean = 1;
 
     protected $data = array('Blink 182' => array(
     	array(
@@ -36,12 +37,17 @@ class WordCloudTest extends PHPUnit_Framework_TestCase
 	    $this->words = array('lots' => new WC_Word('lots', 1),
 	    					'of' => new WC_Word('of',1),
 	    					'small' => new WC_Word('small',1),
-	    					'things' => new WC_Word('things',1));
+	    					'things' => new WC_Word('things',1),
+	    					'i' => new WC_Word('i',2),
+	    					'wish' => new WC_Word('wish',1),
+	    					'was' => new WC_Word('was',1),
+	    					'young' => new WC_Word('young',1),
+	    					'again' => new WC_Word('again',1));
     }
 
 	public function testWordCloud()
 	{
-		$wordcloud = new WordCloud($data);
+		$wordcloud = new WordCloud();
 		$this->assertNotEmpty($wordcloud->stopwords);
 		
 		return $wordcloud;
@@ -58,13 +64,31 @@ class WordCloudTest extends PHPUnit_Framework_TestCase
     	return $wordcloud;
     }
 
+    //set up function for testMergeWordCount()
+    	private function setUpTestMergeWordCount()
+	{
+		$wordcloud = new WordCloud();
+		$wordcloud->mergeData($this->data);
+		return $wordcloud;
+	}
+
    /**
-	 * @depends testWordCloud
+	 * @depends testMergeData
 	 */
-    public function testMergeWordCount(WordCloud $wordcloud) {
+    public function testMergeWordCount() {
+    	$wordcloud = $this->setUpTestMergeWordCount();
     	$wordcloud->mergeWordCount($this->wordcount);
     	
     	$this->assertEquals($wordcloud->words['lots']->freq, $this->words['lots']->freq);
+    	$this->assertEquals($wordcloud->words['of']->freq, $this->words['of']->freq);
+    	$this->assertEquals($wordcloud->words['small']->freq, $this->words['small']->freq);
+    	$this->assertEquals($wordcloud->words['things']->freq, $this->words['things']->freq);
+
+		$this->assertEquals($wordcloud->words['lots']->word, $this->words['lots']->word);
+    	$this->assertEquals($wordcloud->words['of']->word, $this->words['of']->word);
+    	$this->assertEquals($wordcloud->words['small']->word, $this->words['small']->word);
+    	$this->assertEquals($wordcloud->words['things']->word, $this->words['things']->word);
+
 
     	return $wordcloud;
     }
@@ -73,38 +97,73 @@ class WordCloudTest extends PHPUnit_Framework_TestCase
 	/**
 	 * @depends testMergeData
 	 */
-	// public function testCountWordFreq(WordCloud $wordcloud)
-	// {
-	// 	$wordcloud->countWordFreq();
+	public function testCountWordFreq(WordCloud $wordcloud)
+	{
+		$wordcloud->countWordFreq();
 
+		$this->assertEquals($wordcloud->words['lots']->freq, $this->words['lots']->freq);
+    	$this->assertEquals($wordcloud->words['of']->freq, $this->words['of']->freq);
+    	$this->assertEquals($wordcloud->words['small']->freq, $this->words['small']->freq);
+    	$this->assertEquals($wordcloud->words['things']->freq, $this->words['things']->freq);
 
+		$this->assertEquals($wordcloud->words['lots']->word, $this->words['lots']->word);
+    	$this->assertEquals($wordcloud->words['of']->word, $this->words['of']->word);
+    	$this->assertEquals($wordcloud->words['small']->word, $this->words['small']->word);
+    	$this->assertEquals($wordcloud->words['things']->word, $this->words['things']->word);
 
+    	$this->assertEquals($wordcloud->words['i']->freq, $this->words['i']->freq);
+    	$this->assertEquals($wordcloud->words['wish']->freq, $this->words['wish']->freq);
+    	$this->assertEquals($wordcloud->words['was']->freq, $this->words['was']->freq);
+    	$this->assertEquals($wordcloud->words['young']->freq, $this->words['young']->freq);
+    	$this->assertEquals($wordcloud->words['again']->freq, $this->words['again']->freq);
 
-	// 	//Sanity check
-	// 	$this->assertEquals($this->lyrics, $song->lyrics);
+		$this->assertEquals($wordcloud->words['i']->word, $this->words['i']->word);
+    	$this->assertEquals($wordcloud->words['wish']->word, $this->words['wish']->word);
+    	$this->assertEquals($wordcloud->words['was']->word, $this->words['was']->word);
+    	$this->assertEquals($wordcloud->words['young']->word, $this->words['young']->word);
+    	$this->assertEquals($wordcloud->words['again']->word, $this->words['again']->word);
 		
-	// 	$wordcount = $song->getWordCount();
-	// 	$this->assertEquals(1, $wordcount['multiple']);
-	// 	$this->assertEquals(2, $wordcount['lyrics']);
-	// 	$this->assertEquals(2, count($wordcount));
-		
-	// 	return $song;
-	// }
+		return $wordcloud;
+	}
 
-	// public function countWordFreq() {
- //    	try {
-	//         foreach ($this->artists as $name => $artist) {
-	//             foreach ($artist->songs as $title => $song) {
-	//                 $wordCount = $song->getWordCount();
-	//                 // if (Settings->DEBUG) echo "Got this word count from song: ", json_encode($wordCount);
-	//                 $this->mergeWordCount($wordCount);
-	//             }
-	//         }
- //    	} catch (Exception $e) {
- //    		throw $e;
- //    	}
- //    }
-	
+	/**
+	 * @depends testCountWordFreq
+	 */
+	public function testFilterStopwords(WordCloud $wordcloud)
+	{
+		$words = $wordcloud->filter_stopwords();
+		$this->assertArrayHasKey('lots', $words);
+		$this->assertArrayHasKey('small', $words);
+		$this->assertArrayHasKey('things', $words);
+		$this->assertArrayHasKey('young', $words);
+
+		$this->assertArrayNotHasKey('wish', $words);
+		$this->assertArrayNotHasKey('again', $words);
+		$this->assertArrayNotHasKey('of', $words);
+		$this->assertArrayNotHasKey('i', $words);
+		$this->assertArrayNotHasKey('was', $words);
+
+		return $words;
+	}
+
+	/**
+	 * @depends testCountWordFreq
+	 */
+	public function testGenerateWC($words)
+	{
+
+	}
+
+	/**
+	 * @depends testFilterStopwords
+	 */
+	public function testStandardDeviation($words)
+	{
+		$wordcloud = $this->setUpTestMergeWordCount();
+		$stdDev = $wordcloud->standardDeviation($words, $this->mean);
+		$this->assertEquals($stdDev, 0);
+	}
+
 	// /**
 	//  * @depends testGetWordCount
 	//  */
